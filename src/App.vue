908c8a811be39994b89f392af02f18a7438f9a0c
changxianglin <281532612@qq.com>
2019-11-11 23:57:13 +0800

    create class component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <AppNavbar></AppNavbar>
+    <router-view></router-view>
+    <AppFooter></AppFooter>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import {Vue, Component} from 'vue-property-decorator'
+import AppFooter from '@/components/AppFooter.vue'
+import AppNavbar from '@/components/AppNavbar.vue'
 
 @Component({
   components: {
-    HelloWorld,
-  },
+    AppFooter,
+    AppNavbar
+  }
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+
+}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
